12637d6f21ec40a06bc42d2b719dc49b5082e9ab
brad-morrison <bradley_morrison@outlook.com>
2023-05-05 00:13:39 +0100

    new update

--- SoundsOfScotland-iOS/Components/MediumCard.swift
+++ SoundsOfScotland-iOS/Components/MediumCard.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct MediumCard: View {
     var soundscape: Soundscape
-    
+    // gg
     var body: some View {
         Button {} label:  {
             HStack(spacing: 20) {
